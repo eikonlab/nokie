4d0275ec857f4afed63f174b0412042f3eb9cf4e
Caroline Buttet <carolinebuttet@gmail.com>
2021-05-24 19:31:23 +0200

    use csv for card and display image

--- src/components/CardDeck.vue
+++ src/components/CardDeck.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
+  <div class="card-container">
     <h2>{{ subject }}</h2>
     <div>
-      <div class="carte" v-for="card in cards" :key="card.id">
+      <div class="card" v-for="card in cards" :key="card.id">
         Carte: {{ card.id }}<br />
         {{ card.text }}
-        <img :src="`${card.image}`"  alt="">
-
+        <img :src="require(`@/assets/images/${card.image}`)" alt=""/>
+        <div>
+          <button>A toi de jouer!</button>
+          <button>A toi de jouer!</button>
+        </div>
       </div>
     </div>
   </div>
@@ -18,8 +21,8 @@ export default {
   name: 'CardDeck',
   data: function () {
     return {
-      cards: require(`../data/${this.subject}.json`),
-      cards_csv: require(`../data/${this.subject}.csv`),
+      // cards: require(`../data/${this.subject}.json`),
+      cards: require(`../data/${this.subject}.csv`),
     }
   },
   props: {
@@ -39,13 +42,25 @@ ul {
   margin: 0;
   padding: 0;
 }
-.carte {
+.card-container{
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+}
+.card {
+  max-width:300px;
   margin-bottom: 30px;
   border: 1px solid black;
+  display:flex;
+  flex-direction:column;
 }
 
 .left {
   text-align: left;
 }
-
+img{
+  border: 1px solid red;
+  min-width: 12px;
+  min-height:12px;
+}
 </style>
