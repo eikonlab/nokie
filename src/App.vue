a0aef399bf5b9fb163b8944cb658427b91275dd2
lefutoir <lefutoirwastaken@gmail.com>
2021-05-26 17:57:16 +0200

    dev: relocate elements/components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
-  <div id="app">
-      <nav>
-        <h2>step: <!-- {{ card.step }}--></h2>
-        <div class="links">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/game">Game</router-link>
-        </div>
-        <img src="@/assets/var_icons/nokie_menu_notif.svg" alt="">
-      </nav>
+  <div class="app">
     <router-view/>
   </div>
 </template>
@@ -23,36 +15,12 @@ body, html {
   -webkit-font-smoothing: antialiased;
 }
 
-#app {
-  height: calc(100% - 70px);
+.app {
+  height: 100%;
+  width: 100%;
 }
 
 * {
   font-family: 'VT323', monospace;
-}
-
-nav {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 50px;
-  padding: 10px;
-  background-color: #FFFFFF;
-}
-
-/* Text styling */
-
-h2, nav {
-  font-size: 24px;
-  font-weight: normal;
-  -webkit-font-smoothing: antialiased;
-}
-
-nav a, nav a:visited {
-  color: #4D4D4D;
-}
-
-nav a.router-link-exact-active {
-  color: #1DD0FB;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <nav>
+        <h2>step: {{ currentStep }}</h2>
+        <div class="links">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/game">Game</router-link>
+        </div>
+        <img src="@/assets/var_icons/nokie_menu_notif.svg" alt="">
+      </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav.vue',
+  props: ['currentStep'],
+}
+</script>
+
+<style>
+nav {
+  align-self: flex-start;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+  padding: 10px;
+  background-color: #FFFFFF;
+}
+
+/* Text styling */
+
+h2, nav {
+  font-size: 24px;
+  font-weight: normal;
+  -webkit-font-smoothing: antialiased;
+}
+
+nav a, nav a:visited {
+  color: #4D4D4D;
+}
+
+nav a.router-link-exact-active {
+  color: #1DD0FB;
+}
+</style>
