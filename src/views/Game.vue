8ad4d4ac8bd6336d1b6956931f9f7729d883397c
lefutoir <lefutoirwastaken@gmail.com>
2021-06-01 16:37:17 +0200

    html-logic: get component from child

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="game">
     <Nav :currentStep="currentStep" />
-    <CardDeck v-on:changeStep="changeStep"/>
+    <CardDeck v-on:changeStep="changeStep" v-on:showDialogBox="showDialogBox"/>
+    <div v-if="isDialog" class="dialogContainer">
+      <DialogBox :dialogBox="dialogBox" v-on:hideDialogBox="hideDialogBox" :currentStep="currentStep"/>
+    </div>
   </div>
 </template>
 
 <script>
 import CardDeck from '@/components/CardDeck.vue';
 import Nav from '@/components/Nav.vue';
+import DialogBox from '@/components/DialogBox.vue';
 
 export default {
   name: 'Game',
   components: {
     CardDeck,
-    Nav
+    Nav,
+    DialogBox,
   },
   data: function () {
     return {
       currentStep: '',
+      dialogBox: require(`../data/dialogBox.csv`),
+      isDialog: false,
     }
   },
   methods: {
     changeStep (step){
       this.currentStep = step;
+    },
+    showDialogBox() {
+      this.isDialog = true;
+    },
+    hideDialogBox() {
+      this.isDialog = false;
     }
   }
 }
